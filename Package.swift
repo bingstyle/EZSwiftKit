edd6586a0b5b0b6d835da082d2431635bf665ae1
WeiXinbing <183292352@qq.com>
2020-04-15 17:44:22 +0800

    更新 Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "EZSwiftKit",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v9),
+        .watchOS(.v2),
+        .macOS(.v10_10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
