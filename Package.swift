6c2309fd2a7c0d59447ddb4d4866660449fae1bf
WeiXinbing <183292352@qq.com>
2020-04-15 17:08:51 +0800

    更新 Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "EZSwiftKit",
+            path: "Sources",
             dependencies: []),
-        .testTarget(
-            name: "EZSwiftKitTests",
-            dependencies: ["EZSwiftKit"]),
     ]
 )
